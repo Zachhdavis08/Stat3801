82348c16ad784082d96b7cfb16de30e7e479ffe7
Zachhdavis08 <zjd5162@psu.edu>
2024-02-23 04:03:26 -0500

    d

--- Midterm Model.R
+++ Midterm Model.R
@@ -6,7 +6,26 @@ library(dplyr)
 library(tidyverse)
 
 
-train <- read.csv("C://Users//zachh//Desktop//Data//Stat_380_test.csv")
+train <- read.csv("C://Users//zachh//Data//Stat_380_train.csv")
 test <- read.csv("C://Users//zachh//Data//Stat_380_test.csv")
 covar <- read.csv("C://Users//zachh//Data//covar_data.csv")
 example <- read.csv("C://Users//zachh//Data//Example_Sub.csv")
+
+
+group_avg <- train %>%
+  group_by(qc_code) %>%
+  summarize(SalePrice = mean(SalePrice, na.rm = TRUE))
+
+test$ic50_Omicron <- NULL
+
+lm_model <- lm(ic50_Omicron ~ age + centre + dose_2 + dose_3  + priorSxAtFirstVisit + days_sinceDose2 + days_dose12interval + posTest_beforeVisit, data = train)
+summary(lm_model)
+
+test$ic50_Omicron <- predict(lm_model, newdata = test)
+
+submit <- select(test, sample_id, ic50_Omicron)
+
+write_csv(submit, path = "submit.csv")
+
+
+
